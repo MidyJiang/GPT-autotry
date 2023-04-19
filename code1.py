0993c0a7d3f1c13d6bc910e14c17cbcb4b0ef8fb
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-04-20 04:43:06 +0800

    Update code1.py

--- code1.py
+++ code1.py
@@ -5,7 +5,24 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import os
 
+
+
+def write_tree_to_file(path, output_file, level=0):
+    with open(output_file, 'w',encoding='utf8') as f:
+        dir_list = os.listdir(path)
+        for i in range(len(dir_list)):
+            file_path = os.path.join(path, dir_list[i])
+            if os.path.isdir(file_path):
+                f.write("|   " * level + "+---" + dir_list[i] + "/\n")
+                write_tree_to_file(file_path, output_file, level+1)
+            else:
+                f.write("|   " * level + "+---" + dir_list[i] + "\n")
+
 path=os.path.abspath("")
+output_file = 'exchange_rate.csv'
+
+write_tree_to_file(path, output_file)
+
 print(9,path)
 for a,b,c in os.walk(path):
   print(a,b,c)
